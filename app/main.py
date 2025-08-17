db40f4464412ddef394f36306af3905fa23715a6
Ajay <23f1001281@ds.study.iitm.ac.in>
2025-08-17 21:46:15 +0530

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(
     title="Data Analyst Agent",
